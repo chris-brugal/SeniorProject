bcfdaeafb65eb7d52174da93a2f75acccbd39e6d
Julia <June192@gmail.com>
2022-11-03 12:06:15 -0400

    roadtrip generator progress

--- client/src/components/RoadtripGenerator.js
+++ client/src/components/RoadtripGenerator.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import { Card, Typography, Grid } from '@mui/material';
+import { Card, Typography, Box, TextField, CardContent, CardActions, Button } from '@mui/material';
 
 const RoadtripGenerator = () => {
   return (
     <div>
-      <Grid 
-      container
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-      >
-        <Grid item xs={12}>
-        <Typography variant="h3"></Typography>
-        </Grid>
-        <Grid item>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          minHeight="100vh"
+          sx={{ backgroundImage: `url(${"/images/road.jpg"})`}}
+        >
           <Card variant="outlined" sx={{ height: 500, width: 500 }}>
-            <Typography variant="h3">Roadtrip Generator</Typography>
+            <CardContent>
+              <Typography variant="h3">Roadtrip Generator</Typography>
+            </CardContent>
+            <CardActions>
+              <TextField id="est-trip-time" label="Estimated Trip Time" variant="outlined" />
+            </CardActions>
+            <CardActions>
+              <TextField id="num-passengers" label="Number of passengers" variant="outlined" />
+            </CardActions>
+            <CardActions>
+              <Button variant="contained">Submit</Button>
+            </CardActions>
           </Card>
-        </Grid>
-        
-      </Grid>
+        </Box>
     </div>
     
   )
