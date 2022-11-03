368bd9a7cf6e2fb5cac0600a9168dbba4d4be5ec
Julia Le <julia.le@ufl.edu>
2022-11-03 11:38:04 -0400

    frontend progress

--- client/src/components/RoadtripGenerator.js
+++ client/src/components/RoadtripGenerator.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Card, Typography, Grid } from '@mui/material';
 
 const RoadtripGenerator = () => {
   return (
-    <div>RoadtripGenerator</div>
+    <div>
+      <Grid 
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      >
+        <Grid item xs={12}>
+        <Typography variant="h3"></Typography>
+        </Grid>
+        <Grid item>
+          <Card variant="outlined" sx={{ height: 500, width: 500 }}>
+            <Typography variant="h3">Roadtrip Generator</Typography>
+          </Card>
+        </Grid>
+        
+      </Grid>
+    </div>
+    
   )
 }
 
