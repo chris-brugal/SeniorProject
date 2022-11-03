a3505fda00bac8ed326dfd87efb2b43fa7a64871
chris-brugal <58522442+chris-brugal@users.noreply.github.com>
2022-11-03 12:34:10 -0400

    Feature/top artists (#7)
    * api call working for "GetTopArtists" on frontend
    
    * Added artist grid with images, fixed server express endpoints for getTopArtists/Tracks
    
    * fix to server getTopTracks
    added Top Songs and Popover hover for Top Songs
    
    * slight padding changes for card images
    
    Co-authored-by: fu-jo <56369799+fu-jo@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const spotifyWebApi = require("spotify-web-api-node");
-const axios = require("axios")
+const axios = require("axios");
 
 const app = express();
 const port = 8000;
 
-app.use(cors({origin:"*", credentials:true}));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
@@ -28,7 +31,6 @@ app.get("/", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-
   //  Get the "code" value posted from the client-side and get the user's accessToken from the spotify api
   const code = req.body.code;
 
@@ -39,24 +41,21 @@ app.post("/login", (req, res) => {
       // Settign the access and refresh tokens for later use
       spotifyApi.setAccessToken(data.body.access_token);
       spotifyApi.setRefreshToken(data.body.refresh_token);
-      console.log('the access token is ' + data.body.access_token);
+      console.log("the access token is " + data.body.access_token);
 
       // Returning the User's AccessToken in the json formate
       res.json({
         accessToken: data.body.access_token,
-        refreshToken: data.body.refresh_token
+        refreshToken: data.body.refresh_token,
       });
     })
-    .then((res) => res.json())
-    .then((data) => console.log(data))
     .catch((err) => {
-      console.log('could not retrieve access token',err);
+      console.log("could not retrieve access token", err);
       res.sendStatus(400);
     });
 });
 
 app.post("/refresh", (req, res) => {
-
   //  Get the "code" value posted from the client-side and get the user's accessToken from the spotify api
   const code = req.body.code;
 
@@ -68,48 +67,59 @@ app.post("/refresh", (req, res) => {
       // Returning the User's AccessToken in the json formate
       res.json({
         accessToken: data.body.accessToken,
-        refreshToken: data.body.refresh_token
+        refreshToken: data.body.refresh_token,
       });
     })
     .catch((err) => {
-      console.log('could not refresh access token', err);
+      console.log("could not refresh access token", err);
       res.sendStatus(400);
     });
 });
 
 app.get("/getTopArtists", (req, res) => {
-
   axios
-  .get("https://api.spotify.com/v1/me/top/artists", {
+    .get("https://api.spotify.com/v1/me/top/artists", {
       headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + spotifyApi.getAccessToken()
-      }
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + spotifyApi.getAccessToken(),
+      },
     })
     .then((response) => res.json(response.data))
     .catch((err) => {
-      console.log('could not get top artists', err);
+      console.log("could not get top artists", err);
       res.sendStatus(400);
     });
 });
 
 app.get("/getTopTracks", (req, res) => {
-
   axios
-  .get("https://api.spotify.com/v1/me/top/tracks", {
+    .get("https://api.spotify.com/v1/me/top/tracks", {
       headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + spotifyApi.getAccessToken()
-      }
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + spotifyApi.getAccessToken(),
+      },
     })
     .then((response) => res.json(response.data))
     .catch((err) => {
-      console.log('could not get top tracks', err);
+      console.log("could not get top tracks", err);
       res.sendStatus(400);
     });
 });
 
-
+app.get("/getTopTracks", (req, res) => {
+  axios
+    .get("https://api.spotify.com/v1/me/top/tracks", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + spotifyApi.getAccessToken(),
+      },
+    })
+    .then((response) => res.json(response.data))
+    .catch((err) => {
+      console.log("could not get top tracks", err);
+      res.sendStatus(400);
+    });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
